b29149447ef0e11f900f0b213f03e05b5911e24b
dimitris kokkinos <dmt.kokkinos@gmail.com>
2018-04-10 19:11:05 +0300

    solution 16 simple

---
+++ Problems_0_50/P16_PowerDigitSum/PowerDigitSumSolution.cs
@@ -0,0 +1,24 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problems_0_50.P16_PowerDigitSum
+{
+    public class PowerDigitSumSolution : ISolvable
+    {
+        public void Solve()
+        {
+            var res = BigInteger.Pow(2, 1000);
+            int sum = 0;
+            foreach(var r in res.ToString())
+            {
+                sum += Convert.ToInt32( r.ToString() );
+            }
+            Console.WriteLine("sum is " + sum);
+        }
+    }
+}
