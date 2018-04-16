7d4d09af0eab7b599eaa81e831857e2105f5dfc9
dimitris kokkinos <dimitris.kokkinos@softcom-int.com>
2018-04-16 12:43:40 +0300

    solution P23

--- EulerConsole/Program.cs
+++ EulerConsole/Program.cs
@@ -8,6 +8,7 @@ using Problems_0_50.P19_CountingSundays;
 using Problems_0_50.P2_Even_Fibonacci_numbers;
 using Problems_0_50.P20_FactorialDigitSum;
 using Problems_0_50.P22_NamesScores;
+using Problems_0_50.P23_NonAbundantSums;
 using Problems_0_50.P4_Largest_palindrome_product;
 using Problems_0_50.P5_Smallest_multiple;
 using Problems_0_50.P6_Sum_square_difference;
@@ -20,7 +21,7 @@ namespace EulerConsole
 {
     public class Program
     {
-        static ISolvable Solvable => new NamesScoresSolution();
+        static ISolvable Solvable => new NonAbundantSumsSolution();
 
         static void Main(string[] args)
         {
