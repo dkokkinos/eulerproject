e5e2b17bde1df485e38a3283b9ca3eef4edfc6bf
dimitris kokkinos <dmt.kokkinos@gmail.com>
2018-04-20 20:40:49 +0300

    initialize for solution P18

--- EulerConsole/Program.cs
+++ EulerConsole/Program.cs
@@ -1,23 +1,11 @@
 ﻿using Interfaces;
-using Problems_0_50.Largest_prime_factor;
-using Problems_0_50.Multiplesof3and5;
-using Problems_0_50.P11_LargestProductInaGrid;
-using Problems_0_50.P12_HighlyDivisibleTriangularNumber;
-using Problems_0_50.P13_LargeSum;
-using Problems_0_50.P2_Even_Fibonacci_numbers;
-using Problems_0_50.P4_Largest_palindrome_product;
-using Problems_0_50.P5_Smallest_multiple;
-using Problems_0_50.P6_Sum_square_difference;
-using Problems_0_50.P7_10001st_prime;
-using Problems_0_50.P8_Largest_product_in_a_series;
-using Problems_0_50.P9_Special_Pythagorean_triplet;
-using Problems_0_50.Summation_of_primes;
+using Problems_0_50.P15_LatticePaths;
 
 namespace EulerConsole
 {
     public class Program
     {
-        static ISolvable Solvable => new LargeSumSolution();
+        static ISolvable Solvable => new LatticePathsSolution();
 
         static void Main(string[] args)
         {
---
+++ Problems_0_50/P18_MaximumPathSumI/MaximumPathSumISolution.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems_0_50.P18_MaximumPathSumI
+{
+    public class MaximumPathSumISolution : ISolvable
+    {
+        public void Solve()
+        {
+            List<List<int>> piramid = new List<List<int>>();
+            piramid = GeneratePiramid();
+
+            
+
+        }
+
+        private List<List<int>> GeneratePiramid()
+        {
+            return new List<List<int>>()
+            {
+                new List<int>() {               3               },
+                new List<int>() {              7,4              },
+                new List<int>() {             2,4,6              },
+                new List<int>() {            8,5,9,3              }
+            };
+        }
+    }
+}
